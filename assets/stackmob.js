018bb5936373c01f8399e1abc6e0f0179dcec1e3
Magnetic_dud <mail@dandandin.it>
2013-11-09 01:48:36 +0100

    create, read, delete basic functions

--- assets/stackmob.js
+++ assets/stackmob.js
@@ -17,3 +17,26 @@ function comm(action,object,content){
 function update(object,content){
 	comm("PUT",object,content);
 };
+
+function create(object,content){
+	comm("POST",object,content);
+};
+
+function read(object){
+	var http = new XMLHttpRequest();
+    http.open("GET", url+object, true);
+
+    http.setRequestHeader("Accept", "application/vnd.stackmob+json; version="+production);
+    http.setRequestHeader("X-StackMob-API-Key", apikey);
+    http.send();
+};
+
+function deleteIt(object){
+	//this should be tweaked DELETE http://api.stackmob.com/user?username=johndoe
+	var http = new XMLHttpRequest();
+    http.open("DELETE", url+object, true);
+
+    http.setRequestHeader("Accept", "application/vnd.stackmob+json; version="+production);
+    http.setRequestHeader("X-StackMob-API-Key", apikey);
+    http.send();
+};
